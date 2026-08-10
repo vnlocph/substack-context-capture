44c904c58ba3734c26f9350c6a9ab91c30c9a497
vnlocph <pig53can@gmail.com>
2026-08-10 16:40:22 +0700

    Add extension background worker

---
+++ background.js
@@ -0,0 +1,7 @@
+chrome.runtime.onInstalled.addListener(async () => {
+  await chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
+});
+
+chrome.runtime.onStartup.addListener(async () => {
+  await chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
+});
